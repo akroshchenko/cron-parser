53762b9889f0ab409b1d1460a54105bd78d90e24
Andrii Kroshchenko <andrii.kroshchenko01@dixonscarphone.com>
2021-07-30 09:38:37 +0300

    refactore the base structure

--- cron/parser.go
+++ cron/parser.go
@@ -1,5 +1,8 @@
 package cron
 
+// TODO: document functions and structs
+// TOOD: write tests
+// TODO: add check for cases when day of month does get beyound the max days for this month
 import (
 	"fmt"
 	"strconv"
@@ -8,100 +11,220 @@ import (
 
 const supportedFieldCharacters = "0123456789*-/,"
 
-type bounds struct {
-	min uint
-	max uint
-}
+type bounds [2]uint // 0 - min, 1 - max
+
+var (
+	minuteBounds     = bounds{0, 59}
+	hourBounds       = bounds{0, 23}
+	dayOfMonthBounds = bounds{1, 31}
+	monthBounds      = bounds{1, 12}
+	dayOfWeekBounds  = bounds{1, 7}
+)
 
 func (b bounds) String() string {
-	return fmt.Sprintf("[from %d to %d]", b.min, b.max)
+	return fmt.Sprintf("[from %d to %d]", b[0], b[1])
+}
+
+func (b bounds) min() uint {
+	return b[0]
+}
+
+func (b bounds) max() uint {
+	return b[1]
 }
 
 func (b bounds) include(i uint) bool {
-	return i >= b.min && i <= b.max
+	return i >= b[0] && i <= b[1]
 }
 
-type field struct {
-	name    string
-	bounds  bounds
-	ranges  []uint
+type field uint
+
+const (
+	minuteKey field = iota
+	hourKey
+	dayOfMonthKey
+	monthKey
+	dayOfWeekKey
+	commandKey
+)
+
+// Q: is it ok to have type and methods of the same name?
+func (f field) bounds() bounds {
+	switch f {
+	case minuteKey:
+		return minuteBounds
+	case hourKey:
+		return hourBounds
+	case dayOfMonthKey:
+		return dayOfMonthBounds
+	case monthKey:
+		return monthBounds
+	case dayOfWeekKey:
+		return dayOfWeekBounds
+	}
+	return bounds{} // Q: what should I return here?
+}
+
+func (f field) String() string {
+	switch f {
+	case minuteKey:
+		return "minute"
+	case hourKey:
+		return "hour"
+	case dayOfMonthKey:
+		return "day of month"
+	case monthKey:
+		return "month"
+	case dayOfWeekKey:
+		return "day of week"
+	case commandKey:
+		return "command"
+	}
+
+	return "" // Q: what should I return here?
+}
+
+type schedule struct {
+	ranges  [5][]uint
 	command string
 }
 
-func (f field) getFieldName() string {
-	return f.name
-}
-func (f field) getFieldDetails() string {
-	if f.name == "command" {
-		return f.command
-	}
-	return fmt.Sprintf("%v", f.ranges)
-}
-
-type schedule [6]field
-
 func (s schedule) String() string {
 	var result []string
-	for _, f := range s {
-		result = append(result, fmt.Sprintf("%-14s %v", f.getFieldName(), f.getFieldDetails()))
+	for k, r := range s.ranges {
+		result = append(result, fmt.Sprintf("%-14s %v", field(k), strings.Trim(fmt.Sprint(r), "[]")))
 	}
+	result = append(result, fmt.Sprintf("%-14s %v", "command", s.command))
 	return strings.Join(result, "\n")
 }
 
-func Parse(cronStr string) (*schedule, error) {
+func Parse(inputExpr string) (*schedule, error) {
 
-	defaultSchedule := schedule{
-		field{
-			name:   "minutes",
-			bounds: bounds{0, 59},
-		},
-		field{
-			name:   "hours",
-			bounds: bounds{0, 23},
-		},
-		field{
-			name:   "day of month",
-			bounds: bounds{1, 31},
-		},
-		field{
-			name:   "month",
-			bounds: bounds{1, 12},
-		},
-		field{
-			name:   "day of week",
-			bounds: bounds{1, 7},
-		},
-		field{
-			name: "command",
-		},
-	}
+	var scheduleSpec schedule
 
-	if len(cronStr) == 0 {
+	if len(inputExpr) == 0 {
 		return nil, fmt.Errorf("Error: empty expression string")
 	}
 
-	normalizedString := strings.Join(strings.Fields(cronStr), " ")
+	parsedFields := strings.Fields(inputExpr)
 
-	fields := strings.Split(normalizedString, " ")
-	if len(fields) != len(defaultSchedule) {
-		return nil, fmt.Errorf("Error: the number of provided field does not match the specification (should be provided %d fields). Provided: %s", len(defaultSchedule), cronStr)
+	if len(parsedFields) != (len(scheduleSpec.ranges) + 1) {
+		return nil, fmt.Errorf("Error: the number of provided field does not match the specification (should be provided %d fields). Was provided: %s", len(scheduleSpec.ranges)+1, inputExpr)
 	}
 
-	for i := 0; i < len(defaultSchedule); i++ {
-		if defaultSchedule[i].name == "command" {
-			// Do not parse field if is command
-			defaultSchedule[i].command = fields[i]
+	for i := 0; i < len(parsedFields); i++ {
+		if field(i) == commandKey {
+			scheduleSpec.command = parsedFields[commandKey]
 			continue
 		}
-		r, err := getRange(fields[i], defaultSchedule[i].bounds)
+		r, err := getRange(parsedFields[i], field(i).bounds())
 		if err != nil {
-			return nil, fmt.Errorf("Error to get range from field (%s) '%s': %v", defaultSchedule[i].name, fields[i], err)
+			return nil, fmt.Errorf("Error: failed to get range from field '%s': %s with error: %s", field(i), parsedFields[i], err)
 		}
-		defaultSchedule[i].ranges = r
+		scheduleSpec.ranges[i] = make([]uint, 0, len(r))
+		scheduleSpec.ranges[i] = r
 	}
 
-	return &defaultSchedule, nil
+	return &scheduleSpec, nil
 
+}
+
+func getSequenceFromCommaEpr(expr string, b bounds) ([]uint, error) {
+	var r []uint
+
+	sequence := strings.Split(expr, ",")
+	r = make([]uint, len(sequence))
+	for indx, numb := range sequence {
+		number, err := strconv.Atoi(numb)
+		if err != nil {
+			return nil, fmt.Errorf("Cannot convert %s to integer: %s", numb, err)
+		}
+		if !b.include(uint(number)) {
+			return nil, fmt.Errorf("Number %d is not within bounds: %s", number, b)
+		}
+		r[indx] = uint(number)
+	}
+	return r, nil
+}
+
+func getSequenceFromHifenEpr(expr string, b bounds) ([]uint, error) {
+
+	var r []uint
+
+	boarders := strings.Split(expr, "-")
+	if len(boarders) != 2 {
+		return nil, fmt.Errorf("Wrong syntax of using '-', should be 'n-n' where n is a number: %s", expr)
+	}
+	min, err := strconv.Atoi(boarders[0])
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse the min value of the %s", expr)
+	}
+
+	if !b.include(uint(min)) {
+		return nil, fmt.Errorf("Number %d is not within bounds: %s", min, b)
+	}
+
+	max, err := strconv.Atoi(boarders[1])
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse the max value of the %s", expr)
+	}
+
+	if !b.include(uint(max)) {
+		return nil, fmt.Errorf("Number %d is not within bounds: %s", max, b)
+	}
+
+	if min > max {
+		return nil, fmt.Errorf("Value on the left of '-' should be lover then on the right: %v", expr)
+	}
+
+	r = make([]uint, max-min+1)
+	for i := 0; i < len(r); i++ {
+		r[i] = uint(min + i)
+	}
+	return r, nil
+}
+
+func getSequenceFromSlashExpr(expr string, b bounds) ([]uint, error) {
+	var r []uint
+	boarders := strings.Split(expr, "/")
+	if len(boarders) != 2 {
+		return nil, fmt.Errorf("Wrong syntax of using '/', should be 'n/n' where n is a number(first n could be '*'): %s", expr)
+	}
+	var start, step uint
+	if boarders[0] == "*" {
+		start = b.min()
+	} else {
+		parsedStart, err := strconv.Atoi(boarders[0])
+		if err != nil {
+			return nil, fmt.Errorf("Cannot parse %s: %s", expr, err)
+		}
+		if !b.include(uint(parsedStart)) {
+			return nil, fmt.Errorf("Number %d is not within bounds: %s", parsedStart, b)
+		}
+		start = uint(parsedStart)
+	}
+	parsedStep, err := strconv.Atoi(boarders[1])
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse %s: %s", expr, err)
+	}
+	if !b.include(uint(parsedStep)) {
+		return nil, fmt.Errorf("Number %d is not within bounds: %s", parsedStep, b)
+	}
+	step = uint(parsedStep)
+	if step == 0 {
+		return nil, fmt.Errorf("The 'step' in the expression of format 'start/step' cannot be equeal to zero, got %s", expr)
+	}
+
+	r = make([]uint, (b.max()-start)/step+1)
+	current := start
+	for i := 0; i < len(r); i++ {
+		if current > b.max() {
+			break
+		}
+		r[i] = current
+		current += step
+	}
+	return r, nil
 }
 
 func getRange(f string, b bounds) ([]uint, error) {
@@ -114,107 +237,26 @@ func getRange(f string, b bounds) ([]uint, error) {
 	}
 
 	if strings.ContainsRune(f, rune(',')) {
-		sequence := strings.Split(f, ",")
-		r = make([]uint, len(sequence))
-		for indx, numb := range sequence {
-			number, err := strconv.Atoi(numb)
-			if err != nil {
-				return nil, fmt.Errorf("Cannot convert %s to integer: %s", numb, err)
-			}
-			if !b.include(uint(number)) {
-				return nil, fmt.Errorf("Number %d is not within bounds: %s", number, b)
-			}
-			r[indx] = uint(number)
-		}
-		return r, nil
+		return getSequenceFromCommaEpr(f, b)
 	}
 
 	if strings.ContainsRune(f, rune('-')) {
-
-		step := 1
-
-		boarders := strings.Split(f, "-")
-		if len(boarders) != 2 {
-			return nil, fmt.Errorf("Wrong syntax of using '-', should be 'n-n' where n is a number: %s", f)
-		}
-		min, err := strconv.Atoi(boarders[0])
-		if err != nil {
-			return nil, fmt.Errorf("Cannot parse the min value of the %s", f)
-		}
-
-		if !b.include(uint(min)) {
-			return nil, fmt.Errorf("Number %d is not within bounds: %s", min, b)
-		}
-
-		max, err := strconv.Atoi(boarders[1])
-		if err != nil {
-			return nil, fmt.Errorf("Cannot parse the max value of the %s", f)
-		}
-
-		if !b.include(uint(max)) {
-			return nil, fmt.Errorf("Number %d is not within bounds: %s", max, b)
-		}
-
-		if min > max {
-			return nil, fmt.Errorf("Error: value on the left of '-' should be lover then on the right: %v", f)
-		}
-
-		r = make([]uint, ((max-min)/step)+1)
-		for i := 0; i < len(r); i += step {
-			r[i] = uint(min + i)
-		}
-		return r, nil
+		return getSequenceFromHifenEpr(f, b)
 	}
 
 	if strings.ContainsRune(f, rune('/')) {
-		boarders := strings.Split(f, "/")
-		if len(boarders) != 2 {
-			return nil, fmt.Errorf("Wrong syntax of using '/', should be 'n/n' where n is a number(first n could be '*'): %s", f)
-		}
-		var start, step uint
-		if boarders[0] == "*" {
-			start = 0
-		} else {
-			parsedV, err := strconv.Atoi(boarders[0])
-			if err != nil {
-				return nil, fmt.Errorf("Cannot parse %s: %s", f, err)
-			}
-			if !b.include(uint(parsedV)) {
-				return nil, fmt.Errorf("Number %d is not within bounds: %s", parsedV, b)
-			}
-			start = uint(parsedV)
-		}
-		parsedV, err := strconv.Atoi(boarders[1])
-		if err != nil {
-			return nil, fmt.Errorf("Cannot parse %s: %s", f, err)
-		}
-		if !b.include(uint(parsedV)) {
-			return nil, fmt.Errorf("Number %d is not within bounds: %s", parsedV, b)
-		}
-		step = uint(parsedV)
-
-		r = make([]uint, (b.max-start)/step+1)
-		current := start
-		for i := 0; i < len(r); i++ {
-			if current > b.max {
-				break
-			}
-			r[i] = current
-			current += step
-		}
-		return r, nil
+		return getSequenceFromSlashExpr(f, b)
 	}
 
 	if len(f) == 1 && f == "*" {
-		var step uint = 1
-		r = make([]uint, (b.max-b.min)/step+1)
-		current := b.min
+		r = make([]uint, b.max()-b.min())
+		current := b.min()
 		for i := 0; i < len(r); i++ {
-			if current > b.max {
+			if current > b.max() {
 				break
 			}
 			r[i] = current
-			current += step
+			current++
 		}
 		return r, nil
 	}
